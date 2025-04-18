eab39c747e353944c0f915e535fea9797273762a
Mohamed Maged <abomaged9990@gmail.com>
2025-04-18 22:25:11 +0200

    fix conflict

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,7 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "bootstrap";
 import "./style.css";
 
 import { createPinia } from "pinia";
@@ -12,4 +13,3 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).use(router).use(pinia).mount("#app");
-
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,25 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-// 1- import target components
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-
-// 2- define routes
+import productsPage from "../components/productsPage.vue";
+import productDetails from "../components/ProductDetails.vue";
 
 const routes = [
-    { path: "/", name: "home", component: Home },
-    { path: "/login", name: "login", component: Login },
-    {path: "/register", name: "register", component: Register },
-
+  {
+    path: "/",
+    name: "Home",
+    component: productsPage,
+  },
+  {
+    path: "/products",
+    name: "Products",
+    component: productsPage,
+  },
+  {
+    path: "/products/:id",
+    name: "ProductDetail",
+    component: productDetails,
+    props: true,
+  },
 ];
 
-// 3- create router instance
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
-
-// 4- export router instance
 
 export default router;
