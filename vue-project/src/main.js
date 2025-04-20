07ad73a9ee07de737b60b0da031002192f1ddef8
Abdelhameed Mohammed <abdelhameed7mohammed21@gmail.com>
2025-04-20 23:21:15 +0200

    good style without style.css

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import "bootstrap";
-import "./style.css";
+// import "./style.css";
 
 import { createPinia } from "pinia";
 const pinia = createPinia();
